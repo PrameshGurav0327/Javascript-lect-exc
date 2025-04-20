408f27d0543692dfa0ea4aca8fcffaaeaf7f44a6
pramesh gurav <prameshgurav79286@gmail.com>
2025-04-20 14:15:51 +0530

    updated file name

--- index_variables&data types.js
+++ index_variables&data types.js
@@ -17,24 +17,33 @@
 
 // var : variable can be redeclared and updated. A global scope variable
 
-var fullName = "Tony Stark";
-var age = 24;
-var  totalPrice = 99.99;
-
-
-console.log(fullName);
-
-// let : variable cannot be redeclared but can be updated. A block scope variablr
-
-// let fullName = "Tony Stark";
-// let age = 24;
-// let  totalPrice = 99.99;
+// var fullName = "Tony Stark";
+// var age = 24;
+// var  totalPrice = 99.99;
 
 
 // console.log(fullName);
 
+// let : variable cannot be redeclared but can be updated. A block scope variablr
+
+// let fullName = "Tony Stark";
+// let a;
+// a = 10; // It doesn't work for const
+// age = 20;
+// age = 25;
+// let  totalPrice = 99.99;
+
+
+// console.log(a);
+
 // const : variable cannot be reedeclared or updated. A block scope variable
 
+// const age = 24;
+// age = 20;
+// age = 25;
 
+// const PI = 3.14;
+
+// console.log(PI);
 
 
