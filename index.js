42e3dd99ed2cf7fbc06db09b6de2fd5524007241
pramesh gurav <prameshgurav79286@gmail.com>
2025-04-20 14:00:58 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-console.log("Hello World!")
+// console.log("Welcome to my website");
+// console.log("pramesh gurav");
+
+// name = "Tony Stark";
+// console.log("name");
+
+// fullName = "Tony Stark";
+// age = 24;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// isFollow = true;
+// console.log(isFollow);
+
+
+// Variables - let,const and var
+
+// var : variable can be redeclared and updated. A global scope variable
+
+var fullName = "Tony Stark";
+var age = 24;
+var  totalPrice = 99.99;
+
+
+console.log(fullName);
+
+// let : variable cannot be redeclared but can be updated. A block scope variablr
+
+// let fullName = "Tony Stark";
+// let age = 24;
+// let  totalPrice = 99.99;
+
+
+// console.log(fullName);
+
+// const : variable cannot be reedeclared or updated. A block scope variable
+
+
 
 
